39d38d99da86ca7919352a07a7e4f64b18f82607
Luc Hermkens <hermkensluc@gmail.com>
2019-11-01 16:54:24 +0100

    added support for async/await & class properties

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var envPreset = require('@babel/preset-env')
+var classProperties = require('@babel/plugin-proposal-class-properties')
 var optionalChaining = require('@babel/plugin-proposal-optional-chaining')
 var moduleResolver = require('babel-plugin-module-resolver')
 
 module.exports = function (context, options = {}) {
   return {
-    presets: [envPreset],
+    presets: [[envPreset, {
+      corejs: 3,
+      useBuiltIns: 'usage'
+    }]],
     plugins: [
+      classProperties,
       optionalChaining,
       [moduleResolver, {
         root: ['./src'],
