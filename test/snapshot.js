bbccc1e5f565bb6e5d35c636ad2c004261258d4d
Luc Hermkens <hermkensluc@gmail.com>
2019-11-24 12:39:33 +0100

    updated dependencies

--- test/snapshot.js
+++ test/snapshot.js
@@ -25,11 +25,7 @@ var _test = _interopRequireDefault(require("./src/components/test"));
 
 var _foo$foo;
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
-
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -55,35 +51,25 @@ var wait = function wait() {
   });
 };
 
-var testAsync =
-/*#__PURE__*/
-function () {
-  var _ref = _asyncToGenerator(
-  /*#__PURE__*/
-  regeneratorRuntime.mark(function _callee() {
-    return regeneratorRuntime.wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            _context.t0 = console;
-            _context.next = 3;
-            return wait();
+var testAsync = function testAsync() {
+  return regeneratorRuntime.async(function testAsync$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.t0 = console;
+          _context.next = 3;
+          return regeneratorRuntime.awrap(wait());
 
-          case 3:
-            _context.t1 = _context.sent;
+        case 3:
+          _context.t1 = _context.sent;
 
-            _context.t0.log.call(_context.t0, _context.t1);
+          _context.t0.log.call(_context.t0, _context.t1);
 
-          case 5:
-          case "end":
-            return _context.stop();
-        }
+        case 5:
+        case "end":
+          return _context.stop();
       }
-    }, _callee);
-  }));
-
-  return function testAsync() {
-    return _ref.apply(this, arguments);
-  };
-}();
+    }
+  });
+};
 `.trim()
